aaa715e583f354c7071285c436c8fab1d3eacf0e
Serhat Sevim <serhatsevim01@gmail.com>
2022-10-06 00:08:42 +0300

    N Layered Architecture With Java OOp

---
+++ src/oopWithNLayeredApp/dataAccess/ProductDao.java
@@ -0,0 +1,8 @@
+package oopWithNLayeredApp.dataAccess;
+
+import oopWithNLayeredApp.entities.Product;
+
+public interface ProductDao {
+	//interface ler sadece metod imzasını barındırabilir
+	void add(Product product);
+}
